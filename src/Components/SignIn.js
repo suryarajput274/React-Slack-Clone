9e5f8a067d56d23de9047860ea86e772fc5a9df0
Suraj <suryarajput274@gmail.com>
2020-09-14 22:43:14 +0530

    SignIn Styling

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export default class SignIn extends Component {
     render() {
         return (
-            <div>
-                Sign In 
+            <div className="signin-form">
+                <h1>Sign In/Sign Up </h1>
+                <button>Sign in with Google/ Signup with Google</button> 
             </div>
         );
     }
